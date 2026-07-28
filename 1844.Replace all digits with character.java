a03a47cd0765e353fdf28866a110d3fcafa38e41
perugugreeshma <perugugreeshma@gmail.com>
2026-07-28 08:44:07 +0530

    Create 1844.Replace all digits with character.java

---
+++ 1844.Replace all digits with character.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String replaceDigits(String s) {
+        char[] arr = s.toCharArray();
+        
+        // Loop through all odd indices
+        for (int i = 1; i < arr.length; i += 2) {
+            // Shift the character at (i - 1) by the digit value at i
+            arr[i] = (char) (arr[i - 1] + (arr[i] - '0'));
+        }
+        
+        return new String(arr);
+    }
+}
